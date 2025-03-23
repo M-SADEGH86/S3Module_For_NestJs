330013235415b65f1306587dcbc18769e651635e
M-SADEGH <m.sadegh0086@gmail.com>
2025-03-23 12:10:02 +0330

    write comments and add env file

--- s3.service.ts
+++ s3.service.ts
@@ -4,8 +4,10 @@ import { ResizeOptionsType, UploadOptionType } from './types/s3.type';
 import * as sharp from 'sharp';
 import { S3Message } from 'src/common/enums/message.enum';
 
+// npm i --save @aws-sdk/client-s3
 @Injectable()
 export class S3Service {
+  // Configurations 
   constructor(private readonly s3Client: S3Client) {
     this.s3Client = new S3Client({
       credentials: {
@@ -16,21 +18,23 @@ export class S3Service {
       endpoint: `https://${process.env.S3_ENDPOINT}`,
     });
   }
-
+  // npm i --save sharp 
+  // resize and convert to .webp format 
   private resized(image: Buffer, options: ResizeOptionsType): Promise<Buffer> {
     const { width, fit = 'cover', height = null } = options;
     return sharp(image).resize(width, height, { fit }).webp({ quality: 90 }).toBuffer();
   }
 
+  // upload single file 
   async uploadOne(image: Express.Multer.File, options: UploadOptionType) {
     const { S3_BUCKET_NAME, S3_ENDPOINT } = process.env;
     const { folder, name, width, fit, height } = options;
     const Key: string = `${folder}/${name}-${Date.now()}.webp`;
     const Body = await this.resized(image.buffer, { width, fit, height });
     const command = new PutObjectCommand({
-      Key,
-      Bucket: process.env.S3_BUCKET_NAME,
-      Body,
+      Key, // image Key
+      Bucket: process.env.S3_BUCKET_NAME, // your bucket name 
+      Body, // file as buffer send
     });
     try {
       const response = await this.s3Client.send(command);
@@ -43,6 +47,7 @@ export class S3Service {
     }
   }
 
+  // upload some files 
   async uploadAll(images: Express.Multer.File[], options: UploadOptionType) {
     const uploads: string[] = [];
     for await (let image of images) {
@@ -52,6 +57,7 @@ export class S3Service {
     return uploads;
   }
 
+  // delete single file 
   async deleteOne(location: string) {
     const { S3_BUCKET_NAME, S3_ENDPOINT } = process.env;
     const slice: number = `https://${S3_BUCKET_NAME}.${S3_ENDPOINT}/`.length;
@@ -70,6 +76,7 @@ export class S3Service {
     }
   }
 
+  // delete some files
   async deleteAll (locations: string[]) {
     for await (let location of locations) {
       await this.deleteOne(location)
